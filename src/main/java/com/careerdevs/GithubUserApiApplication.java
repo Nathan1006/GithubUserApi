e153a3c545db5e15f1db9e3373eb3b7e0846467e
Nathan1006 <nrpetit@outlook.com>
2021-11-30 17:47:25 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/GithubUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUserApiApplication.class, args);
+	}
+
+}
